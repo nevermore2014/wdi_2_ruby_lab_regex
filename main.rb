e51c434beef50b603c4134c9640ce4ff383460cb
nevermore2014 <zhou.hongyu8976@gmail.com>
2014-01-14 20:10:50 -0500

    finally

--- main.rb
+++ main.rb
@@ -1,26 +1,40 @@
 require_relative 'source'
 require 'pry'
 
-def valid_ip_addresses 
+def valid_ip_addresses(text)
+	text.scan(/\d{2,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/)
+end
+
+def valid_mit_ip_addresses(text)
+	text.scan(/[18]{2}\.\d{1,3}\.\d{1,3}\.\d{1,2}/)
+end
+
+def non_mit_ip_addresses(text)
+	text.scan(/\d{3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/)
 end 
 
-def valid_mit_ip_addresses 
+def valid_phone_numbers(text)
+	text.scan(/[2-9]{1}\d{0,2}\-\d{0,3}\-?\d{4}/).uniq
 end
 
-def non_mit_ip_addresses 
-end 
+def area_codes(text)
+	new_text = text.scan(/[6-8]\d{2}\-/).uniq
+	new_text.each do |num|
+		num.slice!('-')
+	end
+	return new_text
 
-def valid_phone_numbers 
 end
 
-def area_codes 
+def email_addresses(text)
+	text.scan(/\w{0,}\@\w{0,}\.\w{0,}/)
 end
 
-def email_addresses 
+def zip_codes(text)
+	text.scan(/\b\d{5}\b/)
+
 end
 
-def zip_codes 
-end
-
-def hex_colors 
+def hex_colors(text)
+	text.scan(/^#\w{6}/)
 end
